d30c4bdc2a6bedb72c3066dfe4d3631cd399e7f2
SeunghoLEE <suhole7728@naver.com>
2022-06-30 22:44:28 +0900

    ADD nickname설정

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,22 +1,38 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener('open', () => {
   console.log('Connected to Server :)');
 });
 
 socket.addEventListener('message', message => {
-  console.log('New message: ', message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from Server :(');
 });
+
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  socket.send(makeMessage('new_message', input.value));
   input.value = '';
 }
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+}
 messageForm.addEventListener('submit', handleSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
