d4e9d801560b7800630c2a28781da0200c2ed814
SeunghoLEE <suhole7728@naver.com>
2022-07-01 22:39:35 +0900

    ADD ws 양방향 통신 (시크릿모드 일반모드 테스트 완료)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -33,6 +33,7 @@ function handleNickSubmit(event) {
   event.preventDefault();
   const input = nickForm.querySelector('input');
   socket.send(makeMessage('nickname', input.value));
+  input.value = '';
 }
 messageForm.addEventListener('submit', handleSubmit);
 nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -24,14 +24,21 @@ const sockets = [];
 
 wss.on('connection', socket => {
   sockets.push(socket);
+  socket['nickname'] = 'Anon';
   console.log('Connected to Browser :)');
   socket.on('close', () => {
     console.log('Disconnected from Browser');
   });
-  socket.on('message', message => {
-    sockets.forEach(aSocket => {
-      aSocket.send(message.toString());
-    });
+  socket.on('message', msg => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach(aSocket => {
+          aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
   });
 });
 
