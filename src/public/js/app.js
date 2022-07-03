0131520a066344e77506eecaee561e3c905bbaad
SeunghoLEE <suhole7728@naver.com>
2022-07-03 23:33:36 +0900

    FIX app.js message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -27,6 +27,9 @@ function handleSubmit(event) {
   const input = messageForm.querySelector('input');
   socket.send(makeMessage('new_message', input.value));
   console.log('input.value:', input.value);
+  const li = document.createElement('li');
+  li.innerText = `You: ${input.value}`;
+  messageList.append(li);
   input.value = '';
 }
 
